558e69eb326516933bca5c1f1480ce556491ab83
rico-coelen <rico.coelen@gmail.com>
2019-04-24 16:32:02 +0200

    intial commit

---
+++ ImpHunter/GameStates/PlayingState.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace ImpHunter {
+    class PlayingState : GameObjectList{
+        Cannon cannon;
+        Crosshair crosshair;
+        Fortress fortress;
+
+        private const int SHOOT_COOLDOWN = 20;
+        private int shootTimer = SHOOT_COOLDOWN;
+
+        /// <summary>
+        /// PlayingState constructor which adds the different gameobjects and lists in the correct order of drawing.
+        /// </summary>
+        public PlayingState() {
+            Add(new SpriteGameObject("spr_background"));
+
+            Add(cannon = new Cannon());
+            cannon.Position = new Vector2(GameEnvironment.Screen.X / 2, 490);
+
+            Add(fortress = new Fortress());
+
+            // Always draw the crosshair last.
+            Add(crosshair = new Crosshair());
+        }
+        
+        /// <summary>
+        /// Updates the PlayingState.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Allows the player to shoot after a cooldown.
+        /// </summary>
+        /// <param name="inputHelper"></param>
+        public override void HandleInput(InputHelper inputHelper) {
+            base.HandleInput(inputHelper);
+
+            shootTimer++;
+
+            if (inputHelper.MouseLeftButtonPressed() && shootTimer > SHOOT_COOLDOWN) {
+                crosshair.Expand(SHOOT_COOLDOWN);
+                shootTimer = 0;
+            }
+        }
+    }
+}
---
+++ ImpHunter/ImpHunter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ImpHunter {
+  
+    public class ImpHunter : GameEnvironment {
+        public ImpHunter() {
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent() {
+            base.LoadContent();
+            Screen = new Point(800, 600);
+            ApplyResolutionSettings();
+
+            // Add the game states and call the first one.
+            GameStateManager.AddGameState("Play", new PlayingState());
+            GameStateManager.SwitchTo("Play");
+        }
+
+    }
+}
